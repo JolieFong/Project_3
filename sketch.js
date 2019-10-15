f3b92138fc2f37554b7329892e4fea4724421ae1
JolieFong <55153294+JolieFong@users.noreply.github.com>
2019-10-15 11:42:44 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,3 +26,16 @@ function draw() {
   ellipse(200, 200, 200, 200);
   
 }
+
+function setup() {
+  createCanvas(1400, 1400);
+}
+function draw() {
+  background(750);
+  
+  ellipse(150, 150, 180, 180);
+  point(80,150);
+  point(150,150);
+  line(50, 50, 100, 50);
+  
+}
